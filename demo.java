d0b8245b961d19dc0ad6aacfb8ba922690023b97
sathyadevops <sathyadevops1@gmail.com>
2022-09-13 04:10:32 +0000

    Java code

---
+++ demo.java
@@ -0,0 +1,12 @@
+class Demo
+{
+  public static void main(...)
+  {
+    s.o.p(" Hello world ! ");
+    s.o.p(" Hello world ! ");
+    s.o.p(" Hello world ! ");
+    s.o.p(" Hello world ! ");
+    s.o.p(" Hello world ! ");
+    s.o.p(" Hello world ! ");
+  }
+}
